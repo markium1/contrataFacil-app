ac073834625a9a385dec60f7800c951c642c3876
mss39 <mss39@aluno.ifal.edu.br>
2023-08-04 09:24:44 -0300

    Added routes

--- src/index.js
+++ src/index.js
@@ -3,10 +3,36 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
+
+// paginas
+import Home from "./routes/Home";
+import Prestadores from "./routes/Prestadores";
+import Servicos from "./routes/Servicos";
+
+const router = createBrowserRouter([
+  {
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/prestadores",
+        element: <Prestadores />,
+      },
+      {
+        path: "/servicos",
+        element: <Servicos />,
+      },
+    ],
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
